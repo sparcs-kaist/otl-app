75eed67aefa9e0a58f07057269ea79bd9b87c0be
박현우 <ghwhsbsb123@gmail.com>
2020-02-07 00:24:03 +0900

    Init commit.

---
+++ android/app/src/main/kotlin/me/blog/ghwhsbsb123/timeplanner_mobile/MainActivity.kt
@@ -0,0 +1,12 @@
+package me.blog.ghwhsbsb123.timeplanner_mobile
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
