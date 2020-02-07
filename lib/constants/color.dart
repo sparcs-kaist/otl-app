0430a5c1948eb6f1b7c6b8ccbc551f987bbf873c
박현우 <ghwhsbsb123@gmail.com>
2020-02-07 19:33:17 +0900

    Basic main page design.

---
+++ lib/constants/color.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const IDENTITY_BAR_COLOR = Color(0xFFE54C65);
+const BACKGROUND_COLOR = Color(0xFFF9F0F0);
+const MENU_COLOR = Color(0xFF333333);
--- lib/home.dart
+++ lib/home.dart
@@ -1,13 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:timeplanner_mobile/constants/color.dart';
 
 class TimeplannerHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("OTL PLUS Mobile"),
+    return SafeArea(
+      child: Material(
+        color: BACKGROUND_COLOR,
+        child: Column(
+          children: <Widget>[
+            _buildAppBar(),
+            Expanded(
+              child: IndexedStack(
+                children: <Widget>[],
+              ),
+            ),
+          ],
+        ),
       ),
-      body: Container(),
+    );
+  }
+
+  Widget _buildAppBar() {
+    return Column(
+      children: <Widget>[
+        Container(
+          color: IDENTITY_BAR_COLOR,
+          height: 5,
+        ),
+        SizedBox(
+          height: 50,
+          child: Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16, 11, 0, 12),
+                child: Image.asset("assets/logo.png"),
+              ),
+              const Spacer(),
+              IconButton(
+                color: MENU_COLOR,
+                icon: const Icon(Icons.menu),
+                padding: const EdgeInsets.symmetric(vertical: 13),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
