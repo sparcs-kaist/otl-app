09e680d876b3a771bb627997250c77ff344e0a7d
박현우 <ghwhsbsb123@gmail.com>
2020-02-07 20:25:01 +0900

    Add CircularProgressIndicator in LoginPage.

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,21 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:provider/provider.dart';
+import 'package:timeplanner_mobile/constants/color.dart';
 import 'package:timeplanner_mobile/constants/url.dart';
 import 'package:timeplanner_mobile/providers/auth_model.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool _isVisible = true;
+
   @override
   Widget build(BuildContext context) {
     final authModel = Provider.of<AuthModel>(context, listen: false);
 
     return SafeArea(
-      child: InAppWebView(
-        initialUrl: LOGIN_URL,
-        initialOptions: InAppWebViewWidgetOptions(),
-        onLoadStop: (controller, url) {
-          if (url.startsWith(MAIN_URL)) authModel.authenticate(MAIN_URL);
-        },
+      child: Material(
+        color: BACKGROUND_COLOR,
+        child: Stack(
+          children: <Widget>[
+            Center(
+              child: const CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation(PRIMARY_COLOR),
+              ),
+            ),
+            Visibility(
+              maintainSize: true,
+              maintainAnimation: true,
+              maintainState: true,
+              visible: _isVisible,
+              child: InAppWebView(
+                initialUrl: LOGIN_URL,
+                initialOptions: InAppWebViewWidgetOptions(),
+                onLoadStart: (controller, url) {
+                  if (url.startsWith(MAIN_URL)) {
+                    setState(() {
+                      _isVisible = false;
+                    });
+                  }
+                },
+                onLoadStop: (controller, url) {
+                  if (url.startsWith(MAIN_URL))
+                    authModel.authenticate(MAIN_URL);
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
