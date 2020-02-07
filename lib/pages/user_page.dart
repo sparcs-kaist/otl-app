961043dda9a680d11cc228de05fc70a4cec6d407
박현우 <ghwhsbsb123@gmail.com>
2020-02-08 00:23:42 +0900

    Cleanup LoginPage code.

--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:timeplanner_mobile/constants/color.dart';
-import 'package:timeplanner_mobile/models/user.dart';
 import 'package:timeplanner_mobile/providers/auth_model.dart';
 import 'package:timeplanner_mobile/widgets/custom_appbar.dart';
 
 class UserPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final user = Provider.of<AuthModel>(context).user;
-
     return SafeArea(
       child: Material(
         child: Column(
@@ -22,14 +19,16 @@ class UserPage extends StatelessWidget {
                 ),
               ],
             ),
-            _buildBody(context, user),
+            _buildBody(context),
           ],
         ),
       ),
     );
   }
 
-  Padding _buildBody(BuildContext context, User user) {
+  Widget _buildBody(BuildContext context) {
+    final user = Provider.of<AuthModel>(context).user;
+
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Card(
@@ -41,96 +40,50 @@ class UserPage extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(bottom: 10.0),
-                child: Text(
-                  "내 정보",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 8.0),
-                child: RichText(
-                  text: TextSpan(
-                    style: Theme.of(context).textTheme.body1,
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: "이름 ",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      TextSpan(text: "${user.firstName} ${user.lastName}"),
-                    ],
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 8.0),
-                child: RichText(
-                  text: TextSpan(
-                    style: Theme.of(context).textTheme.body1,
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: "메일 ",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      TextSpan(text: user.email),
-                    ],
-                  ),
-                ),
-              ),
-              Divider(color: DIVIDER_COLOR),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 10.0),
-                child: Text(
-                  "학사 정보",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 8.0),
-                child: RichText(
-                  text: TextSpan(
-                    style: Theme.of(context).textTheme.body1,
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: "학번 ",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      TextSpan(text: user.studentId),
-                    ],
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 8.0),
-                child: RichText(
-                  text: TextSpan(
-                    style: Theme.of(context).textTheme.body1,
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: "전공 ",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      TextSpan(
-                        text: user.majors
-                            .map((department) => department.name)
-                            .join(", "),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Divider(color: DIVIDER_COLOR),
+              _buildTitle("내 정보"),
+              _buildContent(
+                  context, "이름", "${user.firstName} ${user.lastName}"),
+              _buildContent(context, "메일", user.email),
+              const Divider(color: DIVIDER_COLOR),
+              _buildTitle("학사 정보"),
+              _buildContent(context, "학번", user.studentId),
+              _buildContent(context, "전공",
+                  user.majors.map((department) => department.name).join(", ")),
+              const Divider(color: DIVIDER_COLOR),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildContent(BuildContext context, String name, String value) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: RichText(
+        text: TextSpan(
+          style: Theme.of(context).textTheme.body1,
+          children: <TextSpan>[
+            TextSpan(
+              text: "$name ",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            TextSpan(text: value),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTitle(String title) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10.0),
+      child: Text(
+        title,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.bold,
         ),
       ),
     );
