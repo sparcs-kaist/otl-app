99df92288ba62bc57e8f8336d1692cabf1ea1282
박현우 <ghwhsbsb123@gmail.com>
2020-12-22 21:59:08 +0900

    Implement image export.

--- android/app/src/main/kotlin/me/blog/ghwhsbsb123/timeplanner_mobile/MainActivity.kt
+++ android/app/src/main/kotlin/me/blog/ghwhsbsb123/timeplanner_mobile/MainActivity.kt
@@ -1,12 +1,104 @@
 package me.blog.ghwhsbsb123.timeplanner_mobile
 
-import androidx.annotation.NonNull;
+import android.content.ContentUris
+import android.content.ContentValues
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.MethodCall
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    private val CHANNEL = "me.blog.ghwhsbsb123.timeplanner_mobile"
+
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result ->
+            if (call.method == "writeImageAsBytes") {
+                val fileName = call.argument<String>("fileName")
+                val bytes = call.argument<ByteArray>("bytes")
+                if (fileName != null && bytes != null) {
+                    val path = writeImageAsBytes(fileName, bytes)
+                    if (path != null) {
+                        result.success(path)
+                    } else {
+                        result.error("ERROR", "Cannot write image bytes.", null)
+                    }
+                } else {
+                    result.error("ERROR", "Invalid paramters.", null)
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun writeImageAsBytes(fileName: String, bytes: ByteArray): String? {
+        val values = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME, fileName)
+            put(MediaStore.Images.Media.MIME_TYPE, "image/png")
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            values.put(MediaStore.Images.Media.IS_PENDING, 1)
+        }
+
+        val contentResolver = getContentResolver()
+        val projection = arrayOf(
+            MediaStore.MediaColumns._ID,
+            MediaStore.MediaColumns.DISPLAY_NAME,
+            MediaStore.MediaColumns.RELATIVE_PATH,
+            MediaStore.MediaColumns.DATE_MODIFIED
+        )
+        val selection = "${MediaStore.MediaColumns.RELATIVE_PATH}='${Environment.DIRECTORY_PICTURES}${File.separator}' AND ${MediaStore.MediaColumns.DISPLAY_NAME}='$fileName'"
+        val c = contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            projection, selection, null, null)
+        var imageUri: Uri? = null
+
+        if (c != null && c.count >= 1) {
+            if (c.moveToFirst()) {
+                val id = c.getLong(c.getColumnIndexOrThrow(MediaStore.MediaColumns._ID))
+                imageUri = ContentUris.withAppendedId(
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+            }
+        }
+
+        val item = imageUri ?: contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)!!
+        try {
+            val pdf = contentResolver.openFileDescriptor(item, "w", null)
+            if (pdf != null) {
+                val fos = FileOutputStream(pdf.getFileDescriptor())
+                fos.write(bytes)
+                fos.close()
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    values.clear()
+                    values.put(MediaStore.Images.Media.IS_PENDING, 0)
+                    contentResolver.update(item, values, null, null)
+                }
+
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.setDataAndType(item, "image/png")
+                startActivity(intent)
+                return item.path
+            }
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return null
     }
 }
--- lib/pages/timetable_page.dart
+++ lib/pages/timetable_page.dart
@@ -1,7 +1,15 @@
+import 'dart:io';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:timeplanner_mobile/backdrop.dart';
 import 'package:timeplanner_mobile/constants/color.dart';
+import 'package:timeplanner_mobile/extensions/semester.dart';
 import 'package:timeplanner_mobile/layers/lecture_detail_layer.dart';
 import 'package:timeplanner_mobile/models/lecture.dart';
 import 'package:timeplanner_mobile/models/semester.dart';
@@ -21,7 +29,11 @@ class TimetablePage extends StatefulWidget {
 }
 
 class _TimetablePageState extends State<TimetablePage> {
+  static const MethodChannel _channel =
+      const MethodChannel("me.blog.ghwhsbsb123.timeplanner_mobile");
+
   final _selectedKey = GlobalKey();
+  final _paintKey = GlobalKey();
 
   bool _isSearchOpened = false;
   bool _isExamTime = false;
@@ -67,7 +79,7 @@ class _TimetablePageState extends State<TimetablePage> {
                 ),
               ),
               child: Padding(
-                padding: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
                 child: Column(
                   children: <Widget>[
                     SemesterPicker(
@@ -100,88 +112,106 @@ class _TimetablePageState extends State<TimetablePage> {
                             ]).createShader(
                             bounds.shift(Offset(-bounds.left, -bounds.top))),
                         child: SingleChildScrollView(
-                          child: Timetable(
-                            lectures: (_selectedLecture == null)
-                                ? lectures
-                                : [...lectures, _selectedLecture],
-                            isExamTime: _isExamTime,
-                            builder: (lecture) {
-                              final isSelected = _selectedLecture == lecture;
-                              Key key;
+                          child: RepaintBoundary(
+                            key: _paintKey,
+                            child: Container(
+                              color: Colors.white,
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: Timetable(
+                                lectures: (_selectedLecture == null)
+                                    ? lectures
+                                    : [...lectures, _selectedLecture],
+                                isExamTime: _isExamTime,
+                                builder: (lecture) {
+                                  final isSelected =
+                                      _selectedLecture == lecture;
+                                  Key key;
 
-                              if (isSelected && isFirst) {
-                                key = _selectedKey;
-                                isFirst = false;
-                              }
+                                  if (isSelected && isFirst) {
+                                    key = _selectedKey;
+                                    isFirst = false;
+                                  }
 
-                              return TimetableBlock(
-                                key: key,
-                                lecture: lecture,
-                                isTemp: isSelected,
-                                onTap: () {
-                                  Backdrop.of(context)
-                                      .toggleBackdropLayerVisibility(
-                                          LectureDetailLayer(lecture));
+                                  return TimetableBlock(
+                                    key: key,
+                                    lecture: lecture,
+                                    isTemp: isSelected,
+                                    onTap: () {
+                                      Backdrop.of(context)
+                                          .toggleBackdropLayerVisibility(
+                                              LectureDetailLayer(lecture));
+                                    },
+                                    onLongPress: isSelected
+                                        ? null
+                                        : () async {
+                                            bool result = false;
+
+                                            await showDialog(
+                                              context: context,
+                                              barrierDismissible: false,
+                                              builder: (context) => AlertDialog(
+                                                title: const Text("삭제"),
+                                                content: Text(
+                                                    "'${lecture.title}' 수업을 삭제하시겠습니까?"),
+                                                actions: [
+                                                  TextButton(
+                                                    child: const Text("취소"),
+                                                    onPressed: () {
+                                                      result = false;
+                                                      Navigator.pop(context);
+                                                    },
+                                                  ),
+                                                  TextButton(
+                                                    child: const Text("삭제"),
+                                                    onPressed: () {
+                                                      result = true;
+                                                      Navigator.pop(context);
+                                                    },
+                                                  ),
+                                                ],
+                                              ),
+                                            );
+
+                                            if (result) {
+                                              context
+                                                  .read<TimetableModel>()
+                                                  .updateTimetable(
+                                                      lecture: lecture,
+                                                      delete: true);
+                                            }
+                                          },
+                                  );
                                 },
-                                onLongPress: isSelected
-                                    ? null
-                                    : () async {
-                                        bool result = false;
-
-                                        await showDialog(
-                                          context: context,
-                                          barrierDismissible: false,
-                                          builder: (context) => AlertDialog(
-                                            title: const Text("삭제"),
-                                            content: Text(
-                                                "'${lecture.title}' 수업을 삭제하시겠습니까?"),
-                                            actions: [
-                                              TextButton(
-                                                child: const Text("취소"),
-                                                onPressed: () {
-                                                  result = false;
-                                                  Navigator.pop(context);
-                                                },
-                                              ),
-                                              TextButton(
-                                                child: const Text("삭제"),
-                                                onPressed: () {
-                                                  result = true;
-                                                  Navigator.pop(context);
-                                                },
-                                              ),
-                                            ],
-                                          ),
-                                        );
-
-                                        if (result) {
-                                          context
-                                              .read<TimetableModel>()
-                                              .updateTimetable(
-                                                  lecture: lecture,
-                                                  delete: true);
-                                        }
-                                      },
-                              );
-                            },
+                              ),
+                            ),
                           ),
                         ),
                       ),
                     ),
-                    const Divider(
-                      color: DIVIDER_COLOR,
-                      height: 1.0,
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: const Divider(
+                        color: DIVIDER_COLOR,
+                        height: 1.0,
+                      ),
                     ),
                     Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 12.0),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 8.0,
+                        vertical: 12.0,
+                      ),
                       child: TimetableSummary(
                         lectures: lectures,
                         tempLecture: _selectedLecture,
                       ),
                     ),
-                    const Divider(
-                      color: DIVIDER_COLOR,
-                      height: 1.0,
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: const Divider(
+                        color: DIVIDER_COLOR,
+                        height: 1.0,
+                      ),
                     ),
                   ],
                 ),
@@ -269,6 +299,35 @@ class _TimetablePageState extends State<TimetablePage> {
       color: Colors.white,
       child: Wrap(
         children: <Widget>[
+          ListTile(
+            leading: const Icon(Icons.image),
+            title: const Text("이미지 저장"),
+            onTap: () async {
+              final boundary = _paintKey.currentContext.findRenderObject()
+                  as RenderRepaintBoundary;
+              final image = await boundary.toImage(pixelRatio: 3.0);
+              final byteData =
+                  await image.toByteData(format: ImageByteFormat.png);
+
+              final timetableModel = context.read<TimetableModel>();
+              final fileName =
+                  "${timetableModel.selectedSemester.title.replaceAll(" ", "_")}_${timetableModel.selectedIndex + 1}_${_isExamTime ? "시험" : "수업"}.png";
+
+              if (Platform.isAndroid) {
+                await _channel
+                    .invokeMethod("writeImageAsBytes", <String, dynamic>{
+                  "fileName": fileName,
+                  "bytes": byteData.buffer.asUint8List(),
+                });
+              } else {
+                final directory = await getApplicationDocumentsDirectory();
+                final path = "${directory.path}/$fileName";
+                File(path).writeAsBytesSync(byteData.buffer.asUint8List());
+                OpenFile.open(path);
+              }
+              Navigator.pop(context);
+            },
+          ),
           ListTile(
             leading: const Icon(Icons.content_copy),
             title: const Text("복제"),
