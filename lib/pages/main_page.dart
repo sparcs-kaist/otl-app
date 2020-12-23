65196f14fd79843ee97e06124184bdf6f946a105
박현우 <ghwhsbsb123@gmail.com>
2020-12-24 02:38:12 +0900

    Implement academic schedule.

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,108 +1,192 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:timeplanner_mobile/constants/color.dart';
 import 'package:timeplanner_mobile/constants/url.dart';
 import 'package:timeplanner_mobile/dio_provider.dart';
-import 'package:timeplanner_mobile/models/semester.dart';
 import 'package:timeplanner_mobile/models/timetable.dart';
 import 'package:timeplanner_mobile/providers/info_model.dart';
 import 'package:timeplanner_mobile/widgets/timetable_block.dart';
 import 'package:timeplanner_mobile/widgets/today_timetable.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final now = DateTime.now();
-    final semester = context
-        .select<InfoModel, Semester>((model) => model.semesters.firstWhere(
-              (semester) =>
-                  semester.beginning.isBefore(now) && semester.end.isAfter(now),
-              orElse: () => model.semesters.last,
-            ));
+    final infoModel = context.watch<InfoModel>();
+    final semester = infoModel.semesters.firstWhere(
+      (semester) =>
+          semester.beginning.isBefore(now) && semester.end.isAfter(now),
+      orElse: () => infoModel.semesters.last,
+    );
+    int days, hours, minutes;
+
+    if (infoModel.currentSchedule != null) {
+      final timeDiff =
+          (infoModel.currentSchedule["time"] as DateTime).difference(now);
+      days = timeDiff.inDays;
+      hours = timeDiff.inHours - timeDiff.inDays * 24;
+      minutes = timeDiff.inMinutes - timeDiff.inHours * 60;
+    }
 
     return Container(
       transform: Matrix4.translationValues(0, -51, 0),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Stack(
-              alignment: Alignment.center,
-              children: [
-                Image.asset("assets/bg.4556cdee.jpg"),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(6.0),
-                    boxShadow: <BoxShadow>[
-                      BoxShadow(
-                        color: SHADOW_COLOR,
-                        offset: const Offset(0, 3),
-                      ),
-                    ],
-                    color: Colors.white,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              Image.asset("assets/bg.4556cdee.jpg"),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(6.0),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                      color: SHADOW_COLOR,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                  color: Colors.white,
+                ),
+                margin: const EdgeInsets.all(12.0),
+                padding: const EdgeInsets.all(12.0),
+                child: TextField(
+                  style: const TextStyle(fontSize: 14.0),
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.only(),
+                    isDense: true,
+                    hintText: "검색",
+                    hintStyle: TextStyle(
+                      color: PRIMARY_COLOR,
+                      fontSize: 14.0,
+                    ),
+                    icon: Icon(
+                      Icons.search,
+                      color: PRIMARY_COLOR,
+                    ),
                   ),
-                  margin: const EdgeInsets.all(12.0),
-                  padding: const EdgeInsets.all(12.0),
-                  child: TextField(
-                    style: const TextStyle(fontSize: 14.0),
-                    decoration: const InputDecoration(
-                      border: InputBorder.none,
-                      contentPadding: EdgeInsets.only(),
-                      isDense: true,
-                      hintText: "검색",
-                      hintStyle: TextStyle(
-                        color: PRIMARY_COLOR,
-                        fontSize: 14.0,
-                      ),
-                      icon: Icon(
-                        Icons.search,
-                        color: PRIMARY_COLOR,
+                ),
+              ),
+            ],
+          ),
+          FutureBuilder<Response>(
+            future: semester == null
+                ? null
+                : DioProvider().dio.post(API_TIMETABLE_LOAD_URL, data: {
+                    "year": semester.year,
+                    "semester": semester.semester,
+                  }),
+            builder: (context, snapshot) {
+              Timetable timetable;
+
+              if (snapshot.hasData) {
+                final rawTimetables = snapshot.data.data as List;
+                timetable = Timetable.fromJson(rawTimetables.first);
+              }
+
+              return Container(
+                height: 110,
+                padding: const EdgeInsets.all(8.0),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(6.0),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: TodayTimetable(
+                        lectures: snapshot.hasData ? timetable.lectures : [],
+                        now: now,
+                        builder: (lecture) => TimetableBlock(lecture: lecture),
                       ),
                     ),
                   ),
                 ),
-              ],
-            ),
-            FutureBuilder<Response>(
-              future: semester == null
-                  ? null
-                  : DioProvider().dio.post(API_TIMETABLE_LOAD_URL, data: {
-                      "year": semester.year,
-                      "semester": semester.semester,
-                    }),
-              builder: (context, snapshot) {
-                Timetable timetable;
-
-                if (snapshot.hasData) {
-                  final rawTimetables = snapshot.data.data as List;
-                  timetable = Timetable.fromJson(rawTimetables.first);
-                }
-
-                return Container(
-                  height: 110,
-                  padding: const EdgeInsets.all(8.0),
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(6.0),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: TodayTimetable(
-                          lectures: snapshot.hasData ? timetable.lectures : [],
-                          now: now,
-                          builder: (lecture) =>
-                              TimetableBlock(lecture: lecture),
+              );
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(6.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    RichText(
+                      textAlign: TextAlign.center,
+                      text: TextSpan(
+                        style: const TextStyle(
+                          color: Colors.black87,
+                          fontSize: 13.0,
                         ),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: infoModel.currentSchedule == null
+                                ? "정보 없음"
+                                : "D-$days일 $hours시간 $minutes분",
+                            style: const TextStyle(fontSize: 20.0),
+                          ),
+                          TextSpan(text: "\n"),
+                          TextSpan(
+                            text: infoModel.currentSchedule == null
+                                ? "-"
+                                : infoModel.currentSchedule["title"],
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              height: 1.3,
+                            ),
+                          ),
+                          TextSpan(text: " "),
+                          TextSpan(
+                            text: infoModel.currentSchedule == null
+                                ? ""
+                                : infoModel.currentSchedule["name"],
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          TextSpan(text: " "),
+                          TextSpan(
+                            text: infoModel.currentSchedule == null
+                                ? ""
+                                : DateFormat("yyyy.MM.dd").format(infoModel
+                                    .currentSchedule["time"]
+                                    .toLocal()),
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                );
-              },
+                    const SizedBox(height: 8.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            launch("https://cais.kaist.ac.kr");
+                          },
+                          child: const Text(
+                            "학사시스템 바로가기",
+                            style: TextStyle(
+                              color: PRIMARY_COLOR,
+                              fontSize: 11.0,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/providers/info_model.dart
+++ lib/providers/info_model.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:timeplanner_mobile/constants/url.dart';
 import 'package:timeplanner_mobile/dio_provider.dart';
+import 'package:timeplanner_mobile/extensions/semester.dart';
 import 'package:timeplanner_mobile/models/semester.dart';
 import 'package:timeplanner_mobile/models/user.dart';
 
@@ -10,12 +11,37 @@ enum InfoState {
   done,
 }
 
+const USED_SCHEDULE_FIELDS = [
+  "beginning",
+  "end",
+  "courseRegistrationPeriodStart",
+  "courseRegistrationPeriodEnd",
+  "courseAddDropPeriodEnd",
+  "courseDropDeadline",
+  "courseEvaluationDeadline",
+  "gradePosting",
+];
+
+const SCHEDULE_NAME = {
+  "beginning": "개강",
+  "end": "종강",
+  "courseRegistrationPeriodStart": "수강신청기간 시작",
+  "courseRegistrationPeriodEnd": "수강신청기간 종료",
+  "courseAddDropPeriodEnd": "수강변경기간 종료",
+  "courseDropDeadline": "수강취소 마감",
+  "courseEvaluationDeadline": "강의평가 마감",
+  "gradePosting": "성적게시"
+};
+
 class InfoModel extends ChangeNotifier {
   List<Semester> _semesters;
   List<Semester> get semesters => _semesters;
 
   User _user;
   User get user => _user;
+
+  Map<String, dynamic> _currentSchedule;
+  Map<String, dynamic> get currentSchedule => _currentSchedule;
 
   InfoState _state = InfoState.progress;
   InfoState get state => _state;
@@ -24,6 +50,7 @@ class InfoModel extends ChangeNotifier {
     try {
       _semesters = await getSemesters();
       _user = await getUser();
+      _currentSchedule = getCurrentSchedule();
       _state = InfoState.done;
     } catch (exception) {
       print(exception);
@@ -41,5 +68,26 @@ class InfoModel extends ChangeNotifier {
   Future<User> getUser() async {
     final response = await DioProvider().dio.get(SESSION_INFO_URL);
     return User.fromJson(response.data);
+  }
+
+  Map<String, dynamic> getCurrentSchedule() {
+    final now = DateTime.now();
+    final schedules = _semesters
+        .map((semester) => USED_SCHEDULE_FIELDS.map((field) {
+              final time = semester.toJson()[field];
+              if (time == null) return null;
+              return <String, dynamic>{
+                "title": semester.title,
+                "name": SCHEDULE_NAME[field],
+                "time": time,
+              };
+            }))
+        .fold<Iterable<Map<String, dynamic>>>(
+            <Map<String, dynamic>>[], (acc, val) => [...acc, ...val])
+        .where((e) => e != null)
+        .toList();
+    schedules.sort((a, b) => a["time"].compareTo(b["time"]));
+    return schedules.firstWhere((e) => e["time"].isAfter(now),
+        orElse: () => null);
   }
 }
