06b540b6ef7201aa202f80953fc0311ea16bb447
Seungbin Oh <sboh1214@gmail.com>
2021-08-21 01:15:35 +0900

    Fix #10 (error because of nullable API response)

--- lib/widgets/timetable_summary.dart
+++ lib/widgets/timetable_summary.dart
@@ -55,28 +55,28 @@ class TimetableSummary extends StatelessWidget {
         0,
         (acc, lecture) =>
             acc +
-            ((lecture.reviewNum > 0)
+            ((lecture.reviewNum ?? 0 > 0)
                 ? (lecture.credit + lecture.creditAu)
                 : 0));
     double grade = lectures.fold<double>(
         0,
         (acc, lecture) =>
             acc +
-            ((lecture.reviewNum > 0)
+            ((lecture.reviewNum ?? 0 > 0)
                 ? (lecture.grade * (lecture.credit + lecture.creditAu))
                 : 0));
     double load = lectures.fold<double>(
         0,
         (acc, lecture) =>
             acc +
-            ((lecture.reviewNum > 0)
+            ((lecture.reviewNum ?? 0 > 0)
                 ? (lecture.load * (lecture.credit + lecture.creditAu))
                 : 0));
     double speech = lectures.fold<double>(
         0,
         (acc, lecture) =>
             acc +
-            ((lecture.reviewNum > 0)
+            ((lecture.reviewNum ?? 0 > 0)
                 ? (lecture.speech * (lecture.credit + lecture.creditAu))
                 : 0));
     final tempType =
