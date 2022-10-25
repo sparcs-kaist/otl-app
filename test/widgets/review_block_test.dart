33732cb2832496c7633947fa4ddcaf0e8c6e0d07
star <snaoyam@gmail.com>
2022-10-25 15:58:32 +0900

    Remove review block isSimple test

--- test/widgets/review_block_test.dart
+++ test/widgets/review_block_test.dart
@@ -9,23 +9,9 @@ void main() {
     await tester.pumpWidget(ReviewBlock(review: SampleReview.shared).material);
   });
 
-  testWidgets('hide buttons in ReviewBlock', (WidgetTester tester) async {
+  testWidgets('test buttons in ReviewBlock', (WidgetTester tester) async {
     await tester.pumpWidget(ReviewBlock(
       review: SampleReview.shared,
-      isSimple: true,
-    ).material);
-
-    final likeFinder = find.text('좋아요');
-    final reportFinder = find.text('신고하기');
-
-    expect(likeFinder, findsNothing);
-    expect(reportFinder, findsNothing);
-  });
-
-  testWidgets('show buttons in ReviewBlock', (WidgetTester tester) async {
-    await tester.pumpWidget(ReviewBlock(
-      review: SampleReview.shared,
-      isSimple: false,
     ).material);
 
     final likeFinder = find.text('좋아요');
