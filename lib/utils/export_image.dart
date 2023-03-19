ddedc77af2ef7af37faf4e01b793d528b0cd396e
Seungbin Oh <sboh1214@gmail.com>
2023-03-20 02:29:21 +0900

    chore: add fastlane, change to open_file_safe

--- lib/utils/export_image.dart
+++ lib/utils/export_image.dart
@@ -3,7 +3,7 @@ import 'dart:ui';
 
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
-import 'package:open_file/open_file.dart';
+import 'package:open_file_safe/open_file_safe.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 
