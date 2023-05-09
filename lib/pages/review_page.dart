bc644ccec3606d02026bdbef577c8a05c4760277
장승호 <hoosong0235@kaist.ac.kr>
2023-05-09 23:37:22 +0900

    feat: add liked reviews, hall of fame, modify my reviews, latest reviews

--- lib/pages/review_page.dart
+++ lib/pages/review_page.dart
@@ -15,10 +15,8 @@ class ReviewPage extends StatefulWidget {
 class _ReviewPageState extends State<ReviewPage> {
   final _scrollController = ScrollController();
   int _selectedIndex = 0;
-  int _initialYear = DateTime.now().year - (DateTime.now().month < 7 ? 1 : 0);
-  int _initialSemester = DateTime.now().month < 7 ? 3 : 1;
-  int _selectedYear = DateTime.now().year - (DateTime.now().month < 7 ? 1 : 0);
-  int _selectedSemester = DateTime.now().month < 7 ? 3 : 1;
+  int _selectedYear = 2022;
+  int _selectedSemester = 3;
 
   @override
   Widget build(BuildContext context) {
@@ -60,7 +58,71 @@ class _ReviewPageState extends State<ReviewPage> {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              _buildTab(),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextButton(
+                      onPressed: () {
+                        setState(() {
+                          _scrollController.jumpTo(0);
+                          _selectedIndex = 0;
+                        });
+                      },
+                      style: ButtonStyle(
+                        shape: MaterialStatePropertyAll(
+                          StadiumBorder(),
+                        ),
+                        backgroundColor: _selectedIndex == 0
+                            ? MaterialStatePropertyAll(PRIMARY_COLOR)
+                            : null,
+                      ),
+                      child: Text(
+                        "따끈따끈 과목후기",
+                        style: TextStyle(
+                          color:
+                              _selectedIndex == 0 ? Colors.white : Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: TextButton(
+                      style: ButtonStyle(
+                        shape: MaterialStatePropertyAll(
+                          StadiumBorder(),
+                        ),
+                        backgroundColor: _selectedIndex == 1
+                            ? MaterialStatePropertyAll(PRIMARY_COLOR)
+                            : null,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _scrollController.jumpTo(0);
+                          _selectedIndex = 1;
+                        });
+                      },
+                      child: Text(
+                        "명예의 전당",
+                        style: TextStyle(
+                          color:
+                              _selectedIndex == 1 ? Colors.white : Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              // Text(
+              //   "따끈따끈 과목후기",
+              //   style: const TextStyle(
+              //     fontWeight: FontWeight.bold,
+              //     fontSize: 14.0,
+              //   ),
+              // ),
               const SizedBox(height: 8.0),
               _selectedIndex == 0 ? SizedBox() : _buildSemesterButton(),
               const SizedBox(height: 8.0),
@@ -74,117 +136,53 @@ class _ReviewPageState extends State<ReviewPage> {
     );
   }
 
-  Widget _buildTab() {
-    return Row(
-      children: [
-        Expanded(
-          child: TextButton(
-            onPressed: () {
-              setState(() {
-                _scrollController.jumpTo(0);
-                _selectedIndex = 0;
-              });
-            },
-            style: ButtonStyle(
-              shape: MaterialStatePropertyAll(
-                StadiumBorder(),
-              ),
-              backgroundColor: _selectedIndex == 0
-                  ? MaterialStatePropertyAll(PRIMARY_COLOR)
-                  : null,
-            ),
-            child: Text(
-              "따끈따끈 과목후기",
-              style: TextStyle(
-                color: _selectedIndex == 0 ? Colors.white : Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 14.0,
-              ),
-            ),
-          ),
-        ),
-        Expanded(
-          child: TextButton(
-            style: ButtonStyle(
-              shape: MaterialStatePropertyAll(
-                StadiumBorder(),
-              ),
-              backgroundColor: _selectedIndex == 1
-                  ? MaterialStatePropertyAll(PRIMARY_COLOR)
-                  : null,
-            ),
-            onPressed: () {
-              setState(() {
-                _scrollController.jumpTo(0);
-                _selectedIndex = 1;
-              });
-            },
-            child: Text(
-              "명예의 전당",
-              style: TextStyle(
-                color: _selectedIndex == 1 ? Colors.white : Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 14.0,
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
   Widget _buildSemesterButton() {
-    int length = (_initialYear - 2013) * 2 + (_initialSemester == 1 ? 1 : 2);
-
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       padding: EdgeInsets.fromLTRB(12.0, 0.0, 12.0, 0.0),
       child: Row(
         children: List.generate(
-          length,
-          (index) {
-            int _year = (2013 + index / 2).toInt();
-            int _semester = index % 2 == 0 ? 1 : 3;
-            bool _isSelected =
-                _selectedYear == _year && _selectedSemester == _semester;
-
-            return Row(
-              children: [
-                TextButton(
-                  onPressed: () {
-                    setState(() {
-                      _selectedYear = _year;
-                      _selectedSemester = _semester;
-                      context
-                          .read<HallOfFameModel>()
-                          .clear(_selectedYear, _selectedSemester);
-                    });
-                  },
-                  child: Text(
-                    '$_year ${_semester == 1 ? 'Spring' : 'Fall'}',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: _isSelected ? PRIMARY_COLOR : Colors.black,
-                      fontWeight: FontWeight.normal,
-                      fontSize: 12.0,
-                    ),
-                  ),
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStatePropertyAll(
-                      _isSelected
-                          ? PRIMARY_COLOR.withOpacity(0.2)
-                          : Colors.grey.shade200,
-                    ),
-                    shape: MaterialStatePropertyAll(
-                      StadiumBorder(),
-                    ),
+          20,
+          (index) => Row(
+            children: [
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    _selectedYear = (2022.5 - index / 2).toInt();
+                    _selectedSemester = index % 2 == 1 ? 1 : 3;
+                    context
+                        .read<HallOfFameModel>()
+                        .clear(_selectedYear, _selectedSemester);
+                  });
+                },
+                child: Text(
+                  '${(2022.5 - index / 2).toInt()} ${index % 2 == 1 ? 'Spring' : 'Fall'}',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: (_selectedYear == (2022.5 - index / 2).toInt() &&
+                            _selectedSemester == (index % 2 == 1 ? 1 : 3))
+                        ? PRIMARY_COLOR
+                        : Colors.black,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 12.0,
                   ),
                 ),
-                SizedBox(width: 10),
-              ],
-            );
-          },
-        ).reversed.toList(),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(
+                    (_selectedYear == (2022.5 - index / 2).toInt() &&
+                            _selectedSemester == (index % 2 == 1 ? 1 : 3))
+                        ? PRIMARY_COLOR.withOpacity(0.2)
+                        : Colors.grey.shade200,
+                  ),
+                  shape: MaterialStatePropertyAll(
+                    StadiumBorder(),
+                  ),
+                ),
+              ),
+              SizedBox(width: 10),
+            ],
+          ),
+        ),
       ),
     );
   }
