0a27491824c59b3811232a89bbea2181c97ee15b
장승호 <hoosong0235@kaist.ac.kr>
2023-07-21 19:34:42 +0900

    refactor settings_page.dart

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:otlplus/constants/color.dart';
+import 'package:otlplus/constants/text_styles.dart';
 import 'package:otlplus/providers/settings_model.dart';
 import 'package:otlplus/utils/build_app_bar.dart';
 import 'package:provider/provider.dart';
@@ -14,126 +15,122 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: buildAppBar(context, '설정', false, true),
-        body: Material(
-            child: ListView(children: [
-          ListTile(
-            title: Text(
-              "settings.send_error_log",
-              style: const TextStyle(
-                fontSize: 12.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ).tr(),
-            subtitle: Text(
-              "settings.send_error_log_desc",
-              style: const TextStyle(
-                fontSize: 12.0,
-              ),
-            ).tr(),
-            trailing: PlatformSwitch(
-              value: context.watch<SettingsModel>().getSendCrashlytics(),
-              onChanged: (value) =>
-                  {context.read<SettingsModel>().setSendCrashlytics(value)},
-            ),
-          ),
-          Visibility(
-            visible: context.watch<SettingsModel>().getSendCrashlytics(),
-            child: ListTile(
-              title: Text("settings.send_anonymously",
-                  style: const TextStyle(
-                    fontSize: 12.0,
-                    fontWeight: FontWeight.bold,
-                  )).tr(),
-              subtitle: Text("settings.send_anonymously_desc",
-                  style: const TextStyle(
-                    fontSize: 12.0,
-                  )).tr(),
+      appBar: buildAppBar(context, '설정', false, true),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text(
+                "settings.send_error_log",
+                style: bodyBold,
+              ).tr(),
+              subtitle: Text(
+                "settings.send_error_log_desc",
+                style: bodyRegular,
+              ).tr(),
               trailing: PlatformSwitch(
+                value: context.watch<SettingsModel>().getSendCrashlytics(),
+                onChanged: (value) =>
+                    context.read<SettingsModel>().setSendCrashlytics(value),
+              ),
+            ),
+            Visibility(
+              visible: context.watch<SettingsModel>().getSendCrashlytics(),
+              child: ListTile(
+                title: Text(
+                  "settings.send_anonymously",
+                  style: bodyBold,
+                ).tr(),
+                subtitle: Text(
+                  "settings.send_anonymously_desc",
+                  style: bodyRegular,
+                ).tr(),
+                trailing: PlatformSwitch(
                   value: context
                       .watch<SettingsModel>()
                       .getSendCrashlyticsAnonymously(),
-                  onChanged: (value) => {
-                        context
-                            .read<SettingsModel>()
-                            .setSendCrashlyticsAnonymously(value)
-                      }),
+                  onChanged: (value) => context
+                      .read<SettingsModel>()
+                      .setSendCrashlyticsAnonymously(value),
+                ),
+              ),
             ),
-          ),
-          ListTile(
-            title: Text("settings.reset_all",
-                style: const TextStyle(
-                  fontSize: 12.0,
-                  fontWeight: FontWeight.bold,
-                )).tr(),
-            onTap: () {
-              showDialog(
-                context: context,
-                builder: (context) => AlertDialog(
-                  title: Text('common.alert').tr(),
-                  content: Text('settings.reset_all_desc').tr(),
-                  actions: <Widget>[
-                    TextButton(
-                        child: new Text("common.cancel").tr(),
+            Visibility(
+              visible: kDebugMode,
+              child: ListTile(
+                title: Text(
+                  "settings.throw_test",
+                  style: bodyBold,
+                ).tr(),
+                subtitle: Text(
+                  "settings.throw_test_desc",
+                  style: bodyRegular,
+                ).tr(),
+                onTap: () => throw Exception(),
+              ),
+            ),
+            ListTile(
+              title: Text("settings.reset_all", style: bodyBold).tr(),
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: Text(
+                      'common.alert',
+                      style: titleRegular,
+                    ).tr(),
+                    content: Text(
+                      'settings.reset_all_desc',
+                      style: bodyRegular,
+                    ).tr(),
+                    actions: <Widget>[
+                      TextButton(
+                        child: Text(
+                          "common.cancel",
+                          style: bodyRegular,
+                        ).tr(),
                         onPressed: () {
                           Navigator.pop(context);
-                        }),
-                    TextButton(
-                        child: new Text("common.delete").tr(),
+                        },
+                      ),
+                      TextButton(
+                        child: Text(
+                          "common.delete",
+                          style: bodyRegular,
+                        ).tr(),
                         onPressed: () {
                           context
                               .read<SettingsModel>()
                               .clearAllValues()
                               .then((_) => Navigator.pop(context));
-                        }),
-                  ],
-                ),
-              );
-            },
-          ),
-          Visibility(
-            visible: kDebugMode,
-            child: ListTile(
-              title: Text("settings.throw_test",
-                  style: const TextStyle(
-                    fontSize: 12.0,
-                    fontWeight: FontWeight.bold,
-                  )).tr(),
-              subtitle: Text("settings.throw_test_desc",
-                  style: const TextStyle(
-                    fontSize: 12.0,
-                  )).tr(),
-              onTap: () => throw Exception(),
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
             ),
-          ),
-          AboutListTile(
-            applicationName: "",
-            applicationIcon: Image.asset("assets/logo.png", height: 48.0),
-            aboutBoxChildren: <Widget>[
-              Text(
-                "Online Timeplanner with Lectures Plus @ KAIST",
-                style: const TextStyle(fontSize: 14.0),
-              ),
-              const SizedBox(height: 8.0),
-              Row(
-                children: [
-                  InkWell(
-                    onTap: () => launchUrl(Uri.parse("mailto:$contactEmail")),
-                    child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          contactEmail,
-                          style: const TextStyle(
-                            color: PRIMARY_COLOR,
-                            fontSize: 14.0,
-                            height: 1.3,
-                          ),
-                        )),
-                  )
-                ],
-              )
-            ],
-          ),
-        ])));
+            AboutListTile(
+              applicationName: "",
+              applicationIcon: Image.asset("assets/logo.png", height: 48.0),
+              aboutBoxChildren: <Widget>[
+                Text(
+                  "Online Timeplanner with Lectures Plus @ KAIST",
+                  style: bodyRegular,
+                ),
+                InkWell(
+                  onTap: () => launchUrl(Uri.parse("mailto:$contactEmail")),
+                  child: Text(
+                    contactEmail,
+                    style: bodyRegular.copyWith(color: pinksMain),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
