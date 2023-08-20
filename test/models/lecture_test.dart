160fa11f24afd82caf53bb2bff53149bacc3a4ca
Seungbin Oh <sboh1214@gmail.com>
2023-08-21 02:24:52 +0900

    test: make test code independent with github actions

--- test/models/lecture_test.dart
+++ test/models/lecture_test.dart
@@ -1,46 +1,70 @@
 import 'dart:convert';
-import 'dart:io';
+import 'package:http/http.dart' as http;
 
 import 'package:otlplus/models/lecture.dart';
 import 'package:test/test.dart';
 
 void main() {
+  const semestersUrl =
+      'https://otl.sparcs.org/api/semesters?order=year&order=semester';
+  String lecturesUrl =
+      'https://otl.sparcs.org/api/lectures?year={year}&semester={semester}&keyword=&type={type}&department=ALL&level=ALL&&order=old_code&limit=1';
+
   group('check typeIdx', () {
-    test('BR type index should be 0', () {
-      final lectures =
-          json.decode(File('test_samples/lecture/BR.json').readAsStringSync());
+    setUp(() async {
+      final response = await http.get(Uri.parse(semestersUrl));
+      final body = json.decode(response.body);
+      final year = body[body.length - 1]['year'];
+      final semester = body[body.length - 1]['semester'];
+      lecturesUrl = lecturesUrl
+          .replaceFirst('{year}', year.toString())
+          .replaceFirst('{semester}', semester.toString());
+    });
+
+    test('BR type index should be 0', () async {
+      final url = lecturesUrl.replaceFirst('{type}', 'BR');
+      final response = await http.get(Uri.parse(url));
+      final lectures = json.decode(response.body);
       for (Map<String, dynamic> l in lectures) {
         final Lecture lecture = Lecture.fromJson(l);
         expect(lecture.typeIdx, 0);
       }
     });
-    test('BE type index should be 1', () {
-      final lectures =
-          json.decode(File('test_samples/lecture/BE.json').readAsStringSync());
+
+    test('BE type index should be 1', () async {
+      final url = lecturesUrl.replaceFirst('{type}', 'BE');
+      final response = await http.get(Uri.parse(url));
+      final lectures = json.decode(response.body);
       for (Map<String, dynamic> l in lectures) {
         final Lecture lecture = Lecture.fromJson(l);
         expect(lecture.typeIdx, 1);
       }
     });
-    test('MR type index should be 2', () {
-      final lectures =
-          json.decode(File('test_samples/lecture/MR.json').readAsStringSync());
+
+    test('MR type index should be 2', () async {
+      final url = lecturesUrl.replaceFirst('{type}', 'MR');
+      final response = await http.get(Uri.parse(url));
+      final lectures = json.decode(response.body);
       for (Map<String, dynamic> l in lectures) {
         final Lecture lecture = Lecture.fromJson(l);
         expect(lecture.typeIdx, 2);
       }
     });
-    test('ME type index should be 3', () {
-      final lectures =
-          json.decode(File('test_samples/lecture/ME.json').readAsStringSync());
+
+    test('ME type index should be 3', () async {
+      final url = lecturesUrl.replaceFirst('{type}', 'ME');
+      final response = await http.get(Uri.parse(url));
+      final lectures = json.decode(response.body);
       for (Map<String, dynamic> l in lectures) {
         final Lecture lecture = Lecture.fromJson(l);
         expect(lecture.typeIdx, 3);
       }
     });
-    test('HSE type index should be 4', () {
-      final lectures =
-          json.decode(File('test_samples/lecture/HSE.json').readAsStringSync());
+
+    test('HSE type index should be 4', () async {
+      final url = lecturesUrl.replaceFirst('{type}', 'HSE');
+      final response = await http.get(Uri.parse(url));
+      final lectures = json.decode(response.body);
       for (Map<String, dynamic> l in lectures) {
         final Lecture lecture = Lecture.fromJson(l);
         expect(lecture.typeIdx, 4);
