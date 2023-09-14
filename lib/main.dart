6279091f5b31380db4149f0152efa9fd7d35e9ae
SungyeopJeong <syeopj4a@gmail.com>
2023-09-15 06:09:42 +0900

    chore: remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:otlplus/pages/course_detail_page.dart';
 import 'package:otlplus/pages/lecture_detail_page.dart';
 import 'package:otlplus/pages/liked_review_page.dart';
