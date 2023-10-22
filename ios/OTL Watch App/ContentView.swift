5fd523b8370f4c56ae7dc12f7f8d16a37cb3581f
Soongyu Kwon <me@soongyu.me>
2023-10-22 21:03:30 +0900

    feat: add support watchOS

---
+++ ios/OTL Watch App/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  OTL Watch App
+//
+//  Created by Soongyu Kwon on 10/22/23.
+//  Copyright © 2023 The Chromium Authors. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello OTL!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
