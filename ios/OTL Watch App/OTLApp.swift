c3179d12ea360590793006572f9dde68018ccf45
Soongyu Kwon <me@soongyu.me>
2023-10-22 21:03:30 +0900

    feat: add support watchOS

---
+++ ios/OTL Watch App/OTLApp.swift
@@ -0,0 +1,18 @@
+//
+//  OTLApp.swift
+//  OTL Watch App
+//
+//  Created by Soongyu Kwon on 10/22/23.
+//  Copyright © 2023 The Chromium Authors. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct OTL_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
