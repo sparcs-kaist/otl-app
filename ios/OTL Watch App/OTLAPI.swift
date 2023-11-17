d1319b5ddcd8eaab2104d022cbf15ef9a5609308
Soongyu Kwon <me@soongyu.me>
2023-11-17 16:00:22 +0900

    add: more functions to OTLAPI

--- ios/OTL Watch App/OTLAPI.swift
+++ ios/OTL Watch App/OTLAPI.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Alamofire
+import SwiftUI
 
 struct urls {
     static let BASE_URL = "https://otl.sparcs.org/"
@@ -20,6 +21,49 @@ struct urls {
     static let API_SEMESTER_URL = API_URL + "semesters"
 
 
+}
+
+enum Days: Int {
+    case mon = 0
+    case tue = 1
+    case wed = 2
+    case thu = 3
+    case fri = 4
+    case sat = 5
+    case sun = 6
+}
+
+struct LectureElement: Identifiable, Hashable {
+    let id: Int
+    let title: String
+    let title_en: String
+    let course: Int
+    let old_code: String
+    let class_no: String
+    let year: Int
+    let semester: Int
+    let code: String
+    let department: Int
+    let department_code: String
+    let department_name: String
+    let department_name_en: String
+    let type: String
+    let type_en: String
+    let limit: Int
+    let num_people: Int
+    let is_english: Bool
+    let credit: Int
+    let credit_au: Int
+    let common_title: String
+    let common_title_en: String
+    let class_title: String
+    let class_title_en: String
+    let review_total_weight: Double
+    let grade: Double
+    let speech: Double
+    let professors: [Professor]
+    let classtime: Classtime
+    let examtimes: [Examtime]
 }
 
 struct SemesterElement: Hashable, Codable {
@@ -125,6 +169,29 @@ struct Department: Codable, Hashable {
     let name: String
     let name_en: String
     let code: String
+}
+
+func getColourForCourse(course: Int) -> Color {
+    let colours = [
+        [242.0, 206.0, 206.0],
+        [244.0, 179.0, 174.0],
+        [242.0, 188.0, 160.0],
+        [240.0, 211.0, 171.0],
+        [241.0, 225.0, 169.0],
+        [244.0, 242.0, 179.0],
+        [219.0, 244.0, 190.0],
+        [190.0, 237.0, 215.0],
+        [183.0, 226.0, 222.0],
+        [201.0, 234.0, 244.0],
+        [180.0, 211.0, 237.0],
+        [185.0, 197.0, 237.0],
+        [204.0, 198.0, 237.0],
+        [216.0, 193.0, 240.0],
+        [235.0, 202.0, 239.0],
+        [244.0, 186.0, 219.0]
+    ]
+    
+    return Color(red: Double(colours[course % 16][0]/255), green:Double(colours[course % 16][1]/255), blue:Double(colours[course % 16][2]/255))
 }
 
 class OTLAPI {
