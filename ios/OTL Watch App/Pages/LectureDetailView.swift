6d23387c459f5f16956c74b7076cce0084234ae0
Soongyu Kwon <me@soongyu.me>
2023-11-23 16:55:46 +0900

    fix: minor issues

--- ios/OTL Watch App/Pages/LectureDetailView.swift
+++ ios/OTL Watch App/Pages/LectureDetailView.swift
@@ -8,6 +8,30 @@
 
 import SwiftUI
 
+
+extension UIColor {
+
+    func lighter(by percentage: CGFloat = 30.0) -> UIColor? {
+        return self.adjust(by: abs(percentage) )
+    }
+
+    func darker(by percentage: CGFloat = 30.0) -> UIColor? {
+        return self.adjust(by: -1 * abs(percentage) )
+    }
+
+    func adjust(by percentage: CGFloat = 30.0) -> UIColor? {
+        var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0, alpha: CGFloat = 0
+        if self.getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
+            return UIColor(red: min(red + percentage/100, 1.0),
+                           green: min(green + percentage/100, 1.0),
+                           blue: min(blue + percentage/100, 1.0),
+                           alpha: alpha)
+        } else {
+            return nil
+        }
+    }
+}
+
 extension Color {
     func adjust(hue: CGFloat = 0, saturation: CGFloat = 0, brightness: CGFloat = 0, opacity: CGFloat = 1) -> Color {
         let color = UIColor(self)
@@ -21,6 +45,18 @@ extension Color {
         }
         return self
     }
+    
+    func lighter(by percentage: CGFloat = 30.0) -> Color {
+        let color = UIColor(self)
+        
+        return Color(uiColor: color.lighter(by: percentage)!)
+    }
+    
+    func darker(by percentage: CGFloat = 30.0) -> Color {
+        let color = UIColor(self)
+        
+        return Color(uiColor: color.darker(by: percentage)!)
+    }
 }
 
 struct LectureDetailView: View {
@@ -32,16 +68,16 @@ struct LectureDetailView: View {
             ZStack(alignment: .topLeading) {
                 Rectangle()
                     .ignoresSafeArea()
-                    .foregroundStyle(LinearGradient(colors: [getColourForCourse(course: lecture.course).adjust(brightness: -0.15), getColourForCourse(course: lecture.course).adjust(brightness: -0.75)], startPoint: .top, endPoint: .bottom))
+                    .foregroundStyle(LinearGradient(colors: [getColourForCourse(course: lecture.course).darker(by: 40), getColourForCourse(course: lecture.course).adjust(brightness: -0.8)], startPoint: .top, endPoint: .bottom))
                 VStack(alignment: .leading) {
                     VStack(alignment: .leading) {
-                        Text("\(self.day.rawValue)요일 \(lecture.classtime.begin/60):\(lecture.classtime.begin%60)-\(lecture.classtime.end/60):\(lecture.classtime.end%60)")
+                        Text(String(format: "%@요일 %02d:%02d-%02d:%02d", self.day.rawValue, lecture.classtime.begin/60, lecture.classtime.begin%60, lecture.classtime.end/60, lecture.classtime.end%60))
                             .font(.system(size: 15))
                             .foregroundStyle(getColourForCourse(course: lecture.course).adjust(brightness: 0.5))
                         HStack(alignment: .top) {
                             Circle()
                                 .frame(width: 12, height: 12)
-                                .foregroundStyle(Color.accentColor)
+                                .foregroundStyle(getColourForCourse(course: lecture.course))
                                 .offset(y: 7)
                             Text(lecture.title)
                                 .font(.title3)
@@ -55,6 +91,7 @@ struct LectureDetailView: View {
                         Text("\(lecture.professors[0].name) 교수님")
                             .font(.system(size: 13))
                     }
+                    .offset(y: 15)
                 }.padding()
             }
         }
