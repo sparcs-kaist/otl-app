d20dcfefa7db6f30dc11d963ec8af561b4d7ce27
SungyeopJeong <syeopj4a@gmail.com>
2024-03-08 21:19:42 +0900

    feat: reflect the current time every minute

--- android/app/src/main/java/org/sparcs/otlplus/TodayWidget.kt
+++ android/app/src/main/java/org/sparcs/otlplus/TodayWidget.kt
@@ -1,15 +1,40 @@
 package org.sparcs.otlplus
 
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
+import android.view.View
 import java.util.Calendar
 import android.widget.RemoteViews
+import kotlin.math.roundToInt
 
 /**
  * Implementation of App Widget functionality.
  */
 class TodayWidget : AppWidgetProvider() {
+    companion object {
+        const val ACTION_UPDATE_WIDGET = "ACTION_UPDATE_WIDGET"
+    }
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        super.onReceive(context, intent)
+
+        if (intent?.action == ACTION_UPDATE_WIDGET) {
+            context?.let {
+                val appWidgetManager = AppWidgetManager.getInstance(it)
+                val widget = ComponentName(it, TodayWidget::class.java)
+                val appWidgetIds = appWidgetManager.getAppWidgetIds(widget)
+                for (appWidgetId in appWidgetIds) {
+                    updateTodayWidget(it, appWidgetManager, appWidgetId)
+                }
+            }
+        }
+    }
+
     override fun onUpdate(
         context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray
     ) {
@@ -21,11 +46,40 @@ class TodayWidget : AppWidgetProvider() {
 
     override fun onEnabled(context: Context) {
         // Enter relevant functionality for when the first widget is created
+        super.onEnabled(context)
+        scheduleAlarm(context)
     }
 
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
+        super.onDisabled(context)
+        cancelAlarm(context)
     }
+
+    private fun scheduleAlarm(context: Context) {
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(context, TodayWidget::class.java)
+        intent.action = ACTION_UPDATE_WIDGET
+        val pendingIntent =
+            PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        // Schedule alarm to update widget every minute
+        alarmManager.setRepeating(
+            AlarmManager.RTC, System.currentTimeMillis(), 60000, // 1 minute in milliseconds
+            pendingIntent
+        )
+    }
+
+    private fun cancelAlarm(context: Context) {
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(context, TodayWidget::class.java)
+        intent.action = ACTION_UPDATE_WIDGET
+        val pendingIntent =
+            PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        alarmManager.cancel(pendingIntent)
+    }
+
+
 }
 
 internal fun updateTodayWidget(
@@ -47,6 +101,8 @@ internal fun updateTodayWidget(
     val views = RemoteViews(context.packageName, R.layout.today_widget)
 
     // Add time items
+    views.removeAllViews(R.id.todayTimetable)
+
     val dottedItem = RemoteViews(context.packageName, R.layout.today_dotted_time_item)
     for (time in startHour..endHour) {
         val solidItem = RemoteViews(context.packageName, R.layout.today_solid_time_item)
@@ -56,6 +112,10 @@ internal fun updateTodayWidget(
     }
 
     // Set time indicator
+    fun isVisible(hour: Int, minute: Int): Boolean = (hour + minute / 60.0).let { time ->
+        time > startHour && time < endHour
+    }
+
     fun getMargin(hour: Int, minute: Int): Double =
         startMargin + (hour - startHour + minute / 60.0) * hourMargin
 
@@ -65,11 +125,19 @@ internal fun updateTodayWidget(
     val calendar = Calendar.getInstance()
     val hour = calendar.get(Calendar.HOUR_OF_DAY)
     val minute = calendar.get(Calendar.MINUTE)
-    val margin = getMargin(hour, minute)
-    val offset = getOffset(hour, minute)
+    val visibility = isVisible(hour, minute)
 
-    views.setViewPadding(R.id.todayTimetable, (itemMargin - offset).toInt(), 0, 0, 0)
-    views.setViewPadding(R.id.timeIndicatorContainer, (margin - offset).toInt(), 0, 0, 0)
+    if (visibility) {
+        val margin = getMargin(hour, minute)
+        val offset = getOffset(hour, minute)
+
+        views.setViewPadding(R.id.todayTimetable, (itemMargin - offset).roundToInt(), 0, 0, 0)
+        views.setViewVisibility(R.id.timeIndicator, View.VISIBLE)
+        views.setViewPadding(R.id.timeIndicatorContainer, (margin - offset).roundToInt(), 0, 0, 0)
+    } else {
+        views.setViewPadding(R.id.todayTimetable, itemMargin, 0, 0, 0)
+        views.setViewVisibility(R.id.timeIndicator, View.INVISIBLE)
+    }
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
