be2503ca8c595a9e60cfb9b971acb185a6509acb
edge <j2hyeon02@kaist.ac.kr>
2024-05-24 19:23:22 +0900

    chore: add planner api url

--- lib/constants/url.dart
+++ lib/constants/url.dart
@@ -21,6 +21,9 @@ const API_TIMETABLE_REMOVE_LECTURE_URL =
 const API_LIKED_REVIEW_URL = API_URL + "/users/{user_id}/liked-reviews";
 const API_SHARE_URL = API_URL + "share/timetable/{share_type}";
 
+const API_PLANNER_URL = API_URL + "users/{user_id}/planners";
+const API_PLANNER_ADD_FUTURE_URL = API_PLANNER_URL + "/{planner_id}/add-future-item";
+
 enum ShareType { image, ical }
 
 const CONTACT = "otlplus@sparcs.org";
