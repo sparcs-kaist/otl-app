1c56860798b1291bab63c7bdd2f30dfb399c7967
edge <j2hyeon02@kaist.ac.kr>
2024-05-24 19:25:41 +0900

    feat: change planner design

--- lib/pages/planner_page.dart
+++ lib/pages/planner_page.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:otlplus/constants/text_styles.dart';
+import 'package:otlplus/models/lecture.dart';
+import 'package:otlplus/providers/planner_model.dart';
 import 'package:otlplus/widgets/otl_scaffold.dart';
+import 'package:otlplus/widgets/planner_preview.dart';
 import 'package:otlplus/widgets/planner_select.dart';
+import 'package:otlplus/widgets/planner_semester.dart';
+import 'package:otlplus/widgets/planner_semester_select_tabs.dart';
 import 'package:otlplus/widgets/planner_statistic.dart';
 import 'package:otlplus/widgets/planner_table.dart';
 import 'package:otlplus/widgets/timetable.dart';
@@ -16,45 +22,60 @@ class PlannerPage extends StatefulWidget {
 }
 
 class _PlannerPageState extends State<PlannerPage> {
+
+
   @override
   Widget build(BuildContext context) {
+    final planners = Provider.of<PlannerModel>(context);
+    // final mode =
+    // context.select<PlannerModel, int>((model) => model.user);
+    if(!planners.isLoaded){
+      return OTLLayout(
+        body: Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(),
+              Text(
+                "불러오는 중",
+                style: bodyRegular.copyWith(color: OTLColor.pinksMain),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
     return OTLLayout(
-      leading: PlannerSelect(
-        selectedMode: context.watch<HallOfFameModel>().selectedMode,
-      ),
+      leading: PlannerSelect(),
       trailing: Container(
         padding: EdgeInsets.all(16),
-        child: Row(
-          children: [
-            Icon(
-              Icons.search_outlined,
-              color: OTLColor.pinksMain,
-            ),
-            SizedBox(width: 12,),
-            Icon(
-              Icons.settings_outlined,
-              color: OTLColor.pinksMain,
-            ),
-          ],
+        child: Icon(
+          Icons.settings_outlined,
+          color: OTLColor.pinksMain,
         ),
       ),
       body: Card(
         shape: const RoundedRectangleBorder(
           borderRadius: BorderRadius.only(topRight: Radius.circular(16.0)),
         ),
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(12.0, 12.0, 12.0, 0.0),
-          child: SingleChildScrollView(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
-                PlannerTable(),
-                Container(
-                  color: OTLColor.grayE,
-                  width: double.infinity,
-                  height: 4,
+                // Text("ID : "+planners.planners[planners.selectedIndex].id.toString()),
+                // Text("Start Year : "+planners.planners[planners.selectedIndex].start_year.toString()),
+                // Text("End Year : "+planners.planners[planners.selectedIndex].end_year.toString()),
+                // Text("General Track Total Credit : "+planners.planners[planners.selectedIndex].general_track.total_credit.toString()),
+
+                PlannerPreview(),
+                SizedBox(height: 16,),
+                PlannerSemesterSelectTabs(
+                  onTap: (i){},
                 ),
-                PlannerStatistic()
+                SizedBox(height: 16,),
+                PlannerSemester(),
               ],
             ),
           ),
