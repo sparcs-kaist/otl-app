6a6fe9162d6d7e23a770c2a7220682d00dadf854
Soongyu Kwon <me@soongyu.me>
2024-09-02 18:52:12 +0900

    add: showsChannelTalkButton to preferences

--- lib/providers/settings_model.dart
+++ lib/providers/settings_model.dart
@@ -3,10 +3,12 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 final _kSendCrashlytics = 'sendCrashlytics';
 final _kSendCrashlyticsAnonymously = 'sendCrashlyticsAnonymously';
+final _kShowsChannelTalkButton = 'showsChannelTalkButton';
 
 class SettingsModel extends ChangeNotifier {
   late bool _sendCrashlytics;
   late bool _sendCrashlyticsAnonymously;
+  late bool _showsChannelTalkButton;
 
   bool getSendCrashlytics() => _sendCrashlytics;
   void setSendCrashlytics(bool newValue) {
@@ -24,6 +26,14 @@ class SettingsModel extends ChangeNotifier {
         (instance) => instance.setBool(_kSendCrashlyticsAnonymously, newValue));
   }
 
+  bool getShowsChannelTalkButton() => _showsChannelTalkButton;
+  void setShowsChannelTalkButton(bool newValue) {
+    _showsChannelTalkButton = newValue;
+    notifyListeners();
+    SharedPreferences.getInstance().then(
+        (instance) => instance.setBool(_kShowsChannelTalkButton, newValue));
+  }
+
   SettingsModel({bool forTest = false}) {
     SharedPreferences.getInstance().then((instance) {
       getAllValues(instance);
@@ -32,6 +42,7 @@ class SettingsModel extends ChangeNotifier {
     if (forTest) {
       _sendCrashlytics = true;
       _sendCrashlyticsAnonymously = false;
+      _showsChannelTalkButton = true;
     }
   }
 
@@ -39,6 +50,8 @@ class SettingsModel extends ChangeNotifier {
     _sendCrashlytics = instance.getBool(_kSendCrashlytics) ?? true;
     _sendCrashlyticsAnonymously =
         instance.getBool(_kSendCrashlyticsAnonymously) ?? false;
+    _showsChannelTalkButton =
+        instance.getBool(_kShowsChannelTalkButton) ?? true;
     notifyListeners();
   }
 
