43d923841aa321eefac2416f50b38177f556d230
junsuPark <krpjs0508@gmail.com>
2024-09-03 11:08:39 +0900

    chore: migrate to Kotlin DSL

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,93 @@
+import java.nio.charset.Charset
+import java.util.Properties
+import java.io.FileInputStream
+
+val localProperties = Properties().apply {
+    rootProject.file("local.properties").bufferedReader(Charset.forName("UTF-8"))
+        .use { reader -> load(reader) }
+}
+
+val flutterVersionCode = localProperties.getProperty("flutter.versionCode") ?: "1"
+val flutterVersionName = localProperties.getProperty("flutter.versionName") ?: "1.0"
+
+val keystoreProperties = Properties().apply {
+    val keystorePropertiesFile = rootProject.file("key.properties")
+    val keystorePropertiesFileExists = keystorePropertiesFile.exists()
+    if (keystorePropertiesFileExists) {
+        this.load(FileInputStream(keystorePropertiesFile))
+    }
+}
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "org.sparcs.otlplus"
+    compileSdk = rootProject.extra.get("compileSdkVersion")?.toString()?.toIntOrNull()
+
+    sourceSets {
+        kotlin.sourceSets["main"].kotlin {
+            srcDir("src/main/kotlin")
+        }
+    }
+
+    lint {
+        disable.addAll(
+            listOf(
+                "InvalidPackage",
+            ),
+        )
+    }
+
+    defaultConfig {
+        applicationId = "org.sparcs.otlplus"
+        minSdk = 21
+        targetSdk = rootProject.extra.get("targetSdkVersion")?.toString()?.toIntOrNull()
+        versionCode = flutterVersionCode.toInt()
+        versionName = flutterVersionName
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    signingConfigs {
+        register("release") {
+            if (keystoreProperties.isNotEmpty()) {
+                keyAlias = keystoreProperties.getProperty("keyAlias")!!
+                keyPassword = keystoreProperties.getProperty("keyPassword")!!
+                storeFile = file(keystoreProperties.getProperty("storeFile")!!)
+                storePassword = keystoreProperties.getProperty("storePassword")!!
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+
+    packaging {
+        jniLibs.keepDebugSymbols.add("**/*.so")
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
+dependencies {
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test:runner:1.6.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+}
---
+++ android/settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    val flutterSdkPath = run {
+        val properties = java.util.Properties().apply {
+            file("local.properties")
+                .bufferedReader(java.nio.charset.Charset.forName("UTF-8"))
+                .use { reader -> load(reader) }
+        }
+        properties.getProperty("flutter.sdk")
+            ?: throw GradleException("flutter.sdk not set in local.properties")
+    }
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+}
+
+include(":app")
