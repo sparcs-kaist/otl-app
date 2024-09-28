a56d16b629c0e1cbe50e396cdc9089176fac1477
Ryan Lee <ryan@sparcs.org>
2024-09-28 17:01:29 +0900

    feat: impl design for next_lecture todo: make responsive view & impl timetable and timeline

--- android/app/src/main/java/org/sparcs/otlplus/TimeTableWidget.kt
+++ android/app/src/main/java/org/sparcs/otlplus/TimeTableWidget.kt
@@ -3,12 +3,35 @@ package org.sparcs.otlplus
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.os.Bundle
 import android.widget.RemoteViews
+import org.sparcs.otlplus.api.Data
 
 /**
  * Implementation of App Widget functionality.
  */
+
+enum class WidgetType {
+    NextLecture,
+    Timeline,
+    Timetable,
+}
+
 class TimeTableWidget : AppWidgetProvider() {
+    private var widgetType = WidgetType.NextLecture
+
+    private fun updateWidget(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetId: Int
+    ) {
+        when (widgetType) {
+            WidgetType.NextLecture -> updateNextLecture(context, appWidgetManager, appWidgetId)
+            WidgetType.Timeline -> updateTimeLine(context, appWidgetManager, appWidgetId)
+            WidgetType.Timetable -> updateTimeLine(context, appWidgetManager, appWidgetId)
+        }
+    }
+
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
@@ -16,8 +39,29 @@ class TimeTableWidget : AppWidgetProvider() {
     ) {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId)
+            updateWidget(context, appWidgetManager, appWidgetId)
         }
+    }
+
+    override fun onAppWidgetOptionsChanged(
+        context: Context?,
+        appWidgetManager: AppWidgetManager?,
+        appWidgetId: Int,
+        newOptions: Bundle?
+    ) {
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions)
+
+        val minWidth = newOptions?.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH) ?: 0
+        val minHeight = newOptions?.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT) ?: 0
+
+        widgetType = when {
+            minWidth <= 200 -> WidgetType.NextLecture
+            minHeight > 220 -> WidgetType.Timetable
+            else -> WidgetType.Timeline
+        }
+
+        if (context != null && appWidgetManager != null)
+            updateWidget(context, appWidgetManager, appWidgetId)
     }
 
     override fun onEnabled(context: Context) {
@@ -29,16 +73,34 @@ class TimeTableWidget : AppWidgetProvider() {
     }
 }
 
-internal fun updateAppWidget(
+internal fun updateNextLecture(
     context: Context,
     appWidgetManager: AppWidgetManager,
     appWidgetId: Int
 ) {
-    val widgetText = context.getString(R.string.appwidget_text)
     // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.time_table_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
+    RemoteViews(context.packageName, R.layout.next_lecture_widget).let {
+        it.setTextViewText(R.id.nextLectureDate, Data.nextLectureDate)
+        it.setTextViewText(R.id.nextLectureName, Data.nextLectureName)
+        it.setTextViewText(R.id.nextLecturePlace, Data.nextLecturePlace)
+        it.setTextViewText(R.id.nextLectureProfessor, Data.nextLectureProfessor)
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, it)
+    }
+}
 
-    // Instruct the widget manager to update the widget
-    appWidgetManager.updateAppWidget(appWidgetId, views)
+internal fun updateTimeLine(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetId: Int
+) {
+    RemoteViews(context.packageName, R.layout.next_lecture_widget).let {
+        it.setTextViewText(R.id.nextLecture, "타임라인입니다.")
+        it.setTextViewText(R.id.nextLectureDate, Data.nextLectureDate)
+        it.setTextViewText(R.id.nextLectureName, Data.nextLectureName)
+        it.setTextViewText(R.id.nextLecturePlace, Data.nextLecturePlace)
+        it.setTextViewText(R.id.nextLectureProfessor, Data.nextLectureProfessor)
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, it)
+    }
 }
---
+++ android/app/src/main/java/org/sparcs/otlplus/api/Data.kt
@@ -0,0 +1,15 @@
+package org.sparcs.otlplus.api
+
+object Data { // TODO: Get data using api
+    val nextLectureDate
+        get() = "다음주 월요일"
+
+    val nextLectureName
+        get() = "그래프이론 개론"
+
+    val nextLecturePlace
+        get() = "(E2)산업경영학동1225"
+
+    val nextLectureProfessor
+        get() = "김재훈"
+}
